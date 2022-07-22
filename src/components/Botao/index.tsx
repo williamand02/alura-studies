0d210188e6abbf62949a568854f2a514abd4e6d7
william andrade de souza <williamstrayder@gmail.com>
2022-07-22 17:26:57 -0300

    :recycle: refactor: class para function

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import style from "./Botao.module.scss";
 
-type Props ={
-  children: string,
+interface Props {
   type?: "button" | "submit" | "reset" | undefined,
+  children?: React.ReactNode,
   onClick?: () => void
 }
 
-class Botao extends React.Component<Props> {
-  render(): React.ReactNode {
-    const { type= "button", onClick} = this.props
-    return <button onClick={onClick} type={type} className={style.botao}>{this.props.children}</button>;
-  }
+function Botao({ onClick, type, children }: Props) {
+  return (
+    <button onClick={onClick} type={type} className={style.botao}>
+      {children}
+    </button>
+  );
 }
 
 export default Botao;
--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,46 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { ITarefa } from "../../types/tarefa";
 import Botao from "../Botao";
 import style from './Formulario.module.scss';
 import {v4 as uuidv4} from 'uuid';
 
-class Formulario extends React.Component<{
-  setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
-}> {
-  state ={
-      tarefa:"",
-      tempo:"00:00"
-  }
+interface Props {
+    setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
+}
 
-  adcionarTarefa(event: React.FormEvent<HTMLFormElement>){
+function Formulario ({ setTarefas }:Props){
+  const [tarefa, setTarefa] = useState("");
+  const [tempo, setTempo] = useState("00:00");
+
+  function adcionarTarefa(event: React.FormEvent<HTMLFormElement>){
     event.preventDefault()
-    this.props.setTarefas(tarefasAntigas => 
+    setTarefas(tarefasAntigas => 
       [...tarefasAntigas, 
         {
-          ...this.state,
+          tarefa,
+          tempo,
           selecionado:false,
           completado:false,
           id: uuidv4()
         }
       ]
       );
-    this.setState({
-      tarefa: '',
-      tempo: "00:00"
-    })
+    setTarefa("");
+    setTempo("00:00");
   }
 
-  render(){
     return (
-      <form className={style.novaTarefa} onSubmit={this.adcionarTarefa.bind(this)}>
+      <form className={style.novaTarefa} onSubmit={adcionarTarefa}>
         <div className={style.inputContainer}>
           <label htmlFor="tarefa">Adicione um novo estudo</label>
           <input
             type="text"
             name="tarefa"
             id="tarefa"
-            value={this.state.tarefa}
-            onChange={event=>{this.setState({...this.state,tarefa: event.target.value})}}
+            value={tarefa}
+            onChange={event=>{setTarefa(event.target.value)}}
+            // onChange={event=>{this.setState({...this.state,tarefa: event.target.value})}}
             placeholder="O que você quer estudar"
             required
           />
@@ -52,8 +51,8 @@ class Formulario extends React.Component<{
             step="1"
             name="tempo"
             id="tempo"
-            value={this.state.tempo}
-            onChange ={event=>{this.setState({...this.state, tempo: event.target.value})}}
+            value={tempo}
+            onChange ={event=>{setTempo(event.target.value)}}
             min="00:00:00"
             max="01:30:00"
             required
@@ -62,7 +61,6 @@ class Formulario extends React.Component<{
         <Botao type="submit" >Adcionar</Botao>
       </form>
     );
-  }
 }
 
 export default Formulario;
